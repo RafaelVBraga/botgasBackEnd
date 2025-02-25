ebbce3391517a7bef6e7356cc339ec9b28449b02
Rafael <rafael.vieira.braga@gmail.com>
2025-02-24 21:42:50 -0300

    criação do controller

--- src/main/java/com/rvbraga/controller/BotController.java
+++ src/main/java/com/rvbraga/controller/BotController.java
@@ -2,6 +2,7 @@ package com.rvbraga.controller;
 
 import java.time.LocalDate;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,15 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rvbraga.models.WhatsAppRequest;
+import com.rvbraga.service.WarService;
 
 @RestController
 @RequestMapping("/webhook")
 public class BotController {
+	@Autowired
+	private WarService warService;
+	
 	 @PostMapping("/whatsapp")
 	    public ResponseEntity<String> receberDados(@RequestBody WhatsAppRequest request) {
 	        request.setDataCadastro(LocalDate.now());
-		 	System.out.println("📩 Dados recebidos do WhatsApp:");	        
-	        System.out.println(request);
-	        return ResponseEntity.ok("Dados recebidos com sucesso!");
+		 	System.out.println(" 📩 Dados recebidos do WhatsApp:");	        
+	        warService.save(request);
+	        return ResponseEntity.ok("📩 Dados recebidos com sucesso!");
 	    }
 }
--- src/main/java/com/rvbraga/controller/ViewController.java
+++ src/main/java/com/rvbraga/controller/ViewController.java
@@ -1,13 +1,22 @@
 package com.rvbraga.controller;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.rvbraga.service.WarService;
 
 @Controller
 public class ViewController {
-
-	@GetMapping()
-	public String relatorio() {
-		return 
+	@Autowired
+	private WarService warService;
+	
+	@GetMapping("/relatorio")
+	public String relatorio(Model model) {
+		model.addAttribute("dados",warService.buscarRequisicoes(LocalDate.now()));
+		return "relatorio.html";
 	}
 }
